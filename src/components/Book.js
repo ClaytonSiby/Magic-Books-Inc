43c04a59e63a9083659f5b4ca08f8d78b252df11
ClaytonSiby <claytonsiby@gmail.com>
2021-03-15 18:39:47 +0200

    Style the action buttons for the Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,13 +5,13 @@ import styles from '../assets/book.module.css'
 const Book = ({ id, title, category, removeBook }) => (
   <div className={styles.lessonPanel}>
     <div className='Book-Header'>
-      <div className='School-of'>
+      <div className={styles.category}>
         <p>{category}</p>
       </div>
-      <div className='title'>
+      <div className={styles.title}>
         <h3>{title}</h3>
       </div>
-      <div className='author'>
+      <div className={ styles.author}>
         <p>Suzane Collins</p>
       </div>
       <div className='actions'>
